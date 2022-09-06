b179f462002a5a1508853cb80a15bbaa5f17cd83
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2022-09-05 21:36:28 -0700

    refactor(android): rely on service to push messaging token updates

--- app/src/main/kotlin/com/wardellbagby/tracks/android/auth/AuthenticationChangesWorker.kt
+++ app/src/main/kotlin/com/wardellbagby/tracks/android/auth/AuthenticationChangesWorker.kt
@@ -2,23 +2,16 @@ package com.wardellbagby.tracks.android.auth
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuth.AuthStateListener
-import com.google.firebase.messaging.FirebaseMessaging
 import com.squareup.workflow1.Worker
-import com.wardellbagby.tracks.android.firebase.notifications.NotificationService
-import com.wardellbagby.tracks.models.RegisterNotificationTokenRequest
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class AuthenticationChangesWorker
-@Inject constructor(
-  private val service: NotificationService
-) : Worker<Boolean> {
+@Inject constructor() : Worker<Boolean> {
   override fun run(): Flow<Boolean> {
     return callbackFlow {
       val listener = AuthStateListener {
@@ -27,15 +20,6 @@ class AuthenticationChangesWorker
       FirebaseAuth.getInstance().addAuthStateListener(listener)
       awaitClose { FirebaseAuth.getInstance().removeAuthStateListener(listener) }
     }
-      .onEach {
-        if (it != null) {
-          service.registerNotificationToken(
-            RegisterNotificationTokenRequest(
-              token = FirebaseMessaging.getInstance().token.await()
-            )
-          )
-        }
-      }
       .map { it != null }
       .catch { emit(false) }
   }

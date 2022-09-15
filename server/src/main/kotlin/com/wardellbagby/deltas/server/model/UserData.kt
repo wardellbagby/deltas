1d5a4883eb966d5cb8206ee849d38f2c7456b5e8
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2022-09-14 22:51:15 -0700

    fix(server): allow no message token in the user data response

--- server/src/main/kotlin/com/wardellbagby/deltas/server/model/UserData.kt
+++ server/src/main/kotlin/com/wardellbagby/deltas/server/model/UserData.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class UserData(
-  val messageToken: String?,
-  val createdTrackers: List<String>?,
-  val followedTrackers: List<String>?
+  val messageToken: String? = null,
+  val createdTrackers: List<String>? = null,
+  val followedTrackers: List<String>? = null
 )
